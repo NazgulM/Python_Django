7266b39eafcc94e31a7223e7836a8b4b4f3e44ae
NazgulM <maniyasova.n@gmail.com>
2022-02-22 19:06:18 +0100

    Lesson1

---
+++ com/naza/FirstProject.py
@@ -0,0 +1,26 @@
+print('Hello World')
+print('Python 3 is awesome')
+print(5)
+print(34.5)
+
+city = 'Kathmandu'
+print(city)
+
+destination_city = 'New York'
+city = destination_city
+print(city)
+
+number = 5
+print(number)
+
+pi = 3.14
+number = pi
+print(number)
+
+print('City: ', 'Kathmandu')
+city1 = 'Brussels'
+locations = 3
+print("City: ", city1, ',' 'Locations: ', locations)
+
+
+
