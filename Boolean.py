e04c0fb154ad552725ffc3c7818fb53fdcd66a60
NazgulM <maniyasova.n@gmail.com>
2022-02-24 20:48:14 +0100

    Boolean and operators

---
+++ Boolean.py
@@ -0,0 +1,27 @@
+# boolean logical datatype  - true or false
+result1 = True
+result2 = False
+print(result2)
+
+# comparison operators
+number = 5
+print(number > 10)  # greater than
+number1 = 15
+print(number1 < 16)
+# equal to
+number3 = 10
+print(number3 == 10)
+# != not equal
+number4 = '10'
+print(number4 != 10)
+# <=
+print(number <= 6)
+# logical operators
+age = 22
+gpa = 3.8
+result = age >= 18 and gpa == 3.8
+print(result)
+result3 = age >= 18 or gpa > 4.0
+print(result3)
+result4 = True
+print(not result4 == True)
