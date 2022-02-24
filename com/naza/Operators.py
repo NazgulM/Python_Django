d6df731a33a45aac3ce6d47d99fa4c4d27ad67e8
NazgulM <maniyasova.n@gmail.com>
2022-02-24 19:58:55 +0100

    Some practice

---
+++ com/naza/Operators.py
@@ -0,0 +1,46 @@
+# operators in Python
+name = 'Nazgul'
+# addition
+x = 5
+print(x+10)
+# subtraction
+a = 10
+print(a-4)
+# multiplication
+print(a*55)
+print(x**2) # exponent
+# quotient
+print('Quotient is: ', x//2)
+# remainder
+print('Remainder is: ', x%2)
+
+# operations in parentheses
+number = (45*8) + (5/3)
+print(number)
+
+# join 2 strings
+str1 = 'Hey '
+str2 = 'Jude'
+print(str1 + str2)
+
+# assignment operators
+x, y = 5, 6
+print(x+y)
+x += 10
+print(x)
+x -= 10
+print(x)
+
+fee = 4535
+discount_percent = 10
+discount_amount = (discount_percent / 100) * fee
+discounted_fee = fee - discount_amount
+print('Fee afters discount: ', discounted_fee)
+
+distance_km = 564.5
+# 1 km  = 0.621371
+miles_in_km = 0.621371
+distance_miles = distance_km * miles_in_km
+print('Distance in miles: ', distance_miles)
+
+
---
+++ com/naza/ProgPrac.py
@@ -0,0 +1,23 @@
+number = float(input('Enter a number: '))
+print(type(number))
+# input() always takes input in string format
+# To convert string to int, use int()
+# to convert string to float, use float ()
+# learn comments
+name = input('Enter your name: ')  # take input
+# age = int(input('Enter age: '))
+print('Your name is: ', name)  # print the name
+# print('Your age: ', age)
+
+# print(1)  Windows + /
+# print(2)
+# print(3)
+
+test = '''I am a 
+multiline string'''
+print(test)
+'''This program takes
+ an integer from 
+ a user'''
+
+
