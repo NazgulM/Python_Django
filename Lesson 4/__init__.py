e144388d87cab798eaca8ac4481d32f0abd8d8ba
NazgulM <maniyasova.n@gmail.com>
2022-04-09 07:13:44 +0200

    Global keys

---
+++ Lesson 4/__init__.py
@@ -0,0 +1,36 @@
+print(2**2)
+print(abs(-7))
+print(abs(-7*5))
+print(min(3,4,8,9,0))
+print(max(89, 56, 88, 78))
+print(pow(5,2))
+print(round(456, -1))
+print(round(3.456, 1))
+# 123, 456 -
+# -2 -1 0 1 2 3
+print(16**0.5)
+print(round(1/3, 2))
+a = 4
+print(a+a)
+print(a*5)
+a = 54
+b = 10
+print(a+b)
+age = 20
+print(type(age))
+money = 1.5
+print(type(money))
+a=b=c=r=1
+print(a+b+c+r)
+a=2
+b=7
+b=a
+print(a,b)
+a=b
+print(a,b)
+a=2
+b=7
+t=a
+a=b
+b=t
+print(a,b)
