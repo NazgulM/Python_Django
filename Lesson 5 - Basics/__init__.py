b1a21196312e5ba166baaefeed43b2b044b1305c
NazgulM <maniyasova.n@gmail.com>
2022-04-30 00:24:57 +0200

    Math commands

---
+++ Lesson 5 - Basics/__init__.py
@@ -0,0 +1,48 @@
+import math
+from math import *
+
+print(math.trunc(32))
+print(math.trunc(32.3))
+print(math.trunc(32.9999))
+print(math.trunc(-89.43))
+
+print(math.floor(32.1))
+print(math.floor(33.99))
+print(math.floor(-3.2))
+
+print(math.ceil(4.5))
+print(math.ceil(4.2))
+print(math.ceil(4.000001))
+print(math.ceil(-3.4))
+
+l, w, h = map(int, input().split())
+n = (l + w)*h*2
+print(math.ceil(n/16))
+
+# print(math.ceil(int(input())/10))
+# n = int(input())
+# print(math.ceil(n/10))
+#
+# print(math.ceil(int(input())/4))
+
+
+a = math.ceil(int(input('Number'))/2)
+b = math.ceil(int(input('Number2'))/2)
+c = math.ceil(int(input('Number 3'))/2)
+
+print(a+b+c)
+
+print(sum([math.ceil(int(input()) / 2) for _ in range(3)]))
+
+print(math.ceil(int(input()) / 2) + ceil(int(input()) / 2) + ceil(int(input()) / 2))
+
+class1 = int(input())
+class2 = int(input())
+class3 = int(input())
+print(math.ceil(class1/2)+math.ceil(class2/2)+math.ceil(class3/2))
+
+l, w, h = map(int, input().split())
+print((h * (l + w) + 7) // 8)
+
+
+
