44a279078c874b6351a7c54b8ec7fcca7218ec6f
NazgulM <maniyasova.n@gmail.com>
2022-04-30 10:38:52 +0200

    Math commands

--- Lesson 5 - Basics/__init__.py
+++ Lesson 5 - Basics/__init__.py
@@ -15,9 +15,9 @@ print(math.ceil(4.2))
 print(math.ceil(4.000001))
 print(math.ceil(-3.4))
 
-l, w, h = map(int, input().split())
-n = (l + w)*h*2
-print(math.ceil(n/16))
+# l, w, h = map(int, input().split())
+# n = (l + w)*h*2
+# print(math.ceil(n/16))
 
 # print(math.ceil(int(input())/10))
 # n = int(input())
@@ -26,23 +26,24 @@ print(math.ceil(n/16))
 # print(math.ceil(int(input())/4))
 
 
-a = math.ceil(int(input('Number'))/2)
-b = math.ceil(int(input('Number2'))/2)
-c = math.ceil(int(input('Number 3'))/2)
-
-print(a+b+c)
-
-print(sum([math.ceil(int(input()) / 2) for _ in range(3)]))
-
-print(math.ceil(int(input()) / 2) + ceil(int(input()) / 2) + ceil(int(input()) / 2))
-
-class1 = int(input())
-class2 = int(input())
-class3 = int(input())
-print(math.ceil(class1/2)+math.ceil(class2/2)+math.ceil(class3/2))
-
-l, w, h = map(int, input().split())
-print((h * (l + w) + 7) // 8)
-
-
-
+# a = math.ceil(int(input('Number'))/2)
+# b = math.ceil(int(input('Number2'))/2)
+# c = math.ceil(int(input('Number 3'))/2)
+#
+# print(a+b+c)
+#
+# print(sum([math.ceil(int(input()) / 2) for _ in range(3)]))
+#
+# print(math.ceil(int(input()) / 2) + ceil(int(input()) / 2) + ceil(int(input()) / 2))
+#
+# class1 = int(input())
+# class2 = int(input())
+# class3 = int(input())
+# print(math.ceil(class1/2)+math.ceil(class2/2)+math.ceil(class3/2))
+#
+# l, w, h = map(int, input().split())
+# print((h * (l + w) + 7) // 8)
+#
+print(-53//5)
+print(-47 % (-8))
+print(-150 % 3)
