4bc6416bd95e243b0fd06ee7355acdcd5f9bb578
NazgulM <maniyasova.n@gmail.com>
2022-05-21 23:47:30 +0200

    f string

---
+++ Lesson 5 - Basics/fString.py
@@ -0,0 +1,13 @@
+# print(f'Мое имя {input()}!')
+
+a = input()
+b = int(input())
+print(f'Hello {a.upper()}. You are {b} years old.')
+
+name, age =input(), input()
+print(f'Hello {name.upper()}. You are {age} years old.')
+
+print(f"Hello {input().upper()}. You are {input()} years old.")
+
+a, b = input().upper(), int(input())
+print(f'Hello {a}. You are {b} years old.')
